f21596efa1ecb996698a0922f69863fcafc9cd43
RoxannaW <roxanna_wijtsma@outlook.com>
2020-08-20 13:21:31 +0200

    Merged branches

--- src/api/server.py
+++ src/api/server.py
@@ -4,8 +4,12 @@ import time
 import random
 import json
 
-app = Flask(__name__)
+root_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(root_path)
+print(sys.path)
+from src.utils.folders_tb import open_json
 
+app = Flask(__name__)
 
 
 @app.route("/")
@@ -16,32 +20,35 @@ def default():
 @app.route('/get/Token', methods=['GET'])
 def get_token():
     group_id_number = None
-    N = "C99"
+    N = "C133"
    
     if 'group_id' in request.args:
         group_id_number = str(request.args['group_id'])
     
     if group_id_number == N:
-        return "{'token': B85918591859851}" 
+        return "{'token': C5221570662008902e}" 
         
 
     else:
          return "This is a message of error" + "<br>" + "<br>" + str(request.args)
-        
-    
+
+
+
 @app.route('/get/Json', methods=['GET'])
 def get():
     token_id_number = None
-    S =  "B85918591859851" 
+    S =  "C5221570662008902e" 
+    path = root_path + "\\Resources\\mean.json" 
+    #direction to where json is saved.
     #example, still have to calculate the true value of S
-    json_mean_total_cases = {"d_averages": {"Brazil":433891.216981132,"Iran":92607.858490566,"Mexico":75239.5147058823,"Netherlands":24519.1132075472,"Spain":132212.9099526066}}
+    
     
     if 'token_id' in request.args:
         token_id_number = str(request.args['token_id'])
     
     if token_id_number == S:
 
-        return json_mean_total_cases
+        return open_json(path)
         #Have to add the information that is assigned to our group 
 
     else:
